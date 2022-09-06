d51a80e696f55b6a5cc4cdd051f21583776d05fe
yousef7557 <112934595+yousef7557@users.noreply.github.com>
2022-09-06 12:10:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-8ypen4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-8ypen4)
---
+++ index.ts
@@ -0,0 +1,34 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//####################################################################
+let suu: number;
+let suu1: number;
+let suu2: number;
+let suu3: number;
+suu = 100;
+suu1 = 110;
+suu2 = 120;
+suu3 = 130;
+console.log(suu + suu1 + suu2 + suu3);
+let suui: number;
+suui = 460;
+console.log(suui);
+
+let missi: number;
+let cr7: number;
+missi = 7;
+cr7 = 5;
+console.log(missi - cr7);
+let r9: number;
+r9 = 2;
+console.log(r9);
+
+let bom: number;
+let dom: number;
+bom = 5;
+dom = 6;
+console.log(bom * dom);
+let rom: number;
+rom = 30;
+console.log(rom);
